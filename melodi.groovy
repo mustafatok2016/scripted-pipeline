0a12c17cc07b6fa838e77d97226615c5b318b4ba
mustafatok2016 <mustafa.tk33@gmail.com>
2020-09-28 16:39:13 +0000

    ansible plugin

--- melodi.groovy
+++ melodi.groovy
@@ -8,13 +8,16 @@ node {
     stage("Pull Repo"){
         sh "rm -rf ansible-melodi && git clone https://github.com/ikambarov/ansible-melodi.git"
     }
+    stage("Install Melodi"){
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'ansible-melodi/main.yml'
+    }
 
-    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-        stage("Install Melodi"){
-            sh """
-                export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible-playbook -i "${params.node}," --private-key $SSHKEY ansible-melodi/main.yml -b -u $SSHUSERNAME
-            """
-        }
-    }    
+    // withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+    //     stage("Install Melodi"){
+    //         sh """
+    //             export ANSIBLE_HOST_KEY_CHECKING=False
+    //             ansible-playbook -i "${params.node}," --private-key $SSHKEY ansible-melodi/main.yml -b -u $SSHUSERNAME
+    //         """
+    //     }
+    // }    
 }
