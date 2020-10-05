7591b67358875f1d2189eb1d828b94200ad47067
mustafatok2016 <mustafa.tk33@gmail.com>
2020-10-05 19:48:36 +0000

    added multibranchpacker

--- terraform-ec2.groovy
+++ terraform-ec2.groovy
@@ -23,24 +23,29 @@ else if(params.environment == "prod") {
 }
 
 def tfvar = """
-    s3_bucket = \"jenkins-terraform-mustafa\"
+    s3_bucket = \"jenkins-terraform-evolvecybertraining\"
     s3_folder_project = \"terraform_ec2\"
     s3_folder_region = \"us-east-1\"
     s3_folder_type = \"class\"
     s3_tfstate_file = \"infrastructure.tfstate\"
     environment = \"${params.environment}\"
+
     region   = \"${aws_region_var}\"
     az1      = \"${aws_region_var}a\"
     az2      = \"${aws_region_var}b\"
     az3      = \"${aws_region_var}c\"
+
     vpc_cidr_block  = \"172.32.0.0/16\"
+
     public_cidr1    = \"172.32.1.0/24\"
     public_cidr2    = \"172.32.2.0/24\"
     public_cidr3    = \"172.32.3.0/24\"
+
     private_cidr1   = \"172.32.10.0/24\"
     private_cidr2   = \"172.32.11.0/24\"
     private_cidr3   = \"172.32.12.0/24\"
-    public_key    = ""	
+
+    public_key    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzm79oYWLjj0l8FBah58lwmIF5WeRjNtOq/IeeanU0kMNINwqANnTD580fg3I4vhsVEbLwVbBIQdJGcR0VHNldIhKpK7RWcLJOTWTptKnqSEzoW8VwkSYI0+CoUa+3n4pSGBMNNTGwixstyqaxSBClI+HJQDutPXQMUs+m1N3iLfjTUdCjMF8IKcNXkd/PG8nNxRwhx64hW1Tzgh8g+wGUGMIoqV3onQdPzFooMplZ/jZj8Ow54CA2TIPic50w9TDj2PV752DOmxzeDs6ZpV7kvv1jnijYd3pZNao0UvjhusGthrS6YQ5Dot7GcPEHGyP5OWkQUQFrggxD3wnx/RUJ root@Jenkins-Master"	
     ami_name        = "${params.ami_name}"
 """
 
@@ -85,3 +90,4 @@ node{
         }
     }    
 }
+
